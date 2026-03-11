ec6eb365eadb99d6be71b488c38bb75777f38690
GURU-SEKHAR-REDDY54 <SekharReddy665343@gmail.com>
2026-03-11 12:12:49 +0530

    updated

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "./Content.css"
+
+const API_URL = import.meta.env.VITE_API_URL
+
 function Content() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
   const increment = () => {
     setCount(count + 1);
@@ -10,7 +14,7 @@ function Content() {
     setCount(count - 1);
   };
   const fetchProducts = async () => {
-    const url = "https://backend-app-z2t4.onrender.com/store";
+    const url = `${API_URL}/store`;
     const res = await axios.get(url);
     setProducts(res.data);
   };
@@ -20,15 +24,21 @@ function Content() {
   return (
     <div>
       <h3>Products Page</h3>
-      <button onClick={decrement}>-</button>
+      {/* <button onClick={decrement}>-</button>
       {count}
       <button onClick={increment}>+</button>
-      <hr />
-      <ol>
+      <hr /> */}
+      <div className="row">
         {products.map((product) => (
-          <li>{product.name}</li>
+          <div className="box">
+            <img src={`${API_URL}${product.imageUrl}`} alt="" />
+            <h3>{product.name}</h3>
+            <p>{product.desc}</p>
+            <p>Rs.{product.price}</p>
+            <p><button>Add to cart</button></p>
+          </div>
         ))}
-      </ol>
+      </div>
     </div>
   );
 }
