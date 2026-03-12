1bfa4df8e0ee145bea69089a6204d0a123ebaee8
GURU-SEKHAR-REDDY54 <SekharReddy665343@gmail.com>
2026-03-12 11:44:31 +0530

    updated login,register links

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,29 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
+import Logout from "./components/Logout";
 import Register from "./components/Register";
 import Cart from "./components/Cart";
 import Orders from "./components/Orders";
-import Logout from "./components/Logout";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-export const AppContext= createContext()
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+export const AppContext = createContext();
 function App() {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
   return (
     <div>
-      <AppContext.Provider value={{user, setUser}}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route index element={<Content />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="orders" element={<Orders />} />
-          <Route path="logout" element={<Logout />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <AppContext.Provider value={{ user, setUser }}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route index element={<Content />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="login" element={<Login />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="register" element={<Register />} />
+            <Route path="orders" element={<Orders />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
       </AppContext.Provider>
     </div>
   );
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,44 +5,41 @@ import axios from "axios";
 function Register() {
   const [user, setUser] = useState({});
   const API_URL = import.meta.env.VITE_API_URL;
+  const Navigate = useNavigate();
   const handleSubmit = async () => {
-    const Navigate = useNavigate();
     const url = API_URL + "/auth/signup";
     const response = await axios.post(url, user);
     Navigate("/login");
   };
   return (
     <div>
-      <h2>Register Page</h2>
+      <h2>Registration Page</h2>
       <p>
         <input
           type="text"
           onChange={(e) => setUser({ ...user, name: e.target.value })}
-          placeholder="name"
+          placeholder="Name"
         />
-      </p>{" "}
-      <br />
+      </p>
       <p>
         <input
           type="text"
           onChange={(e) => setUser({ ...user, email: e.target.value })}
           placeholder="Email"
         />
-      </p>{" "}
-      <br />
+      </p>
       <p>
         <input
           type="password"
           onChange={(e) => setUser({ ...user, password: e.target.value })}
           placeholder="Password"
         />
-      </p>{" "}
-      <br />
-      <p>
-        <button>Submit</button>
       </p>
       <p>
-        <Link to="/login">Already a member? Login here.. /</Link>
+        <button onClick={handleSubmit}>Submit</button>
+      </p>
+      <p>
+        <Link to="/login">Already a member? Login here</Link>
       </p>
     </div>
   );
