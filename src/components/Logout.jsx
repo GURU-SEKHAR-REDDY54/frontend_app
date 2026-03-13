a428876cf4ea36594422514bc7125b9d5f6f029d
GURU-SEKHAR-REDDY54 <SekharReddy665343@gmail.com>
2026-03-13 09:46:22 +0530

    updated cart and order

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,8 +1,13 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import App, { AppContext } from "../App";
 function Logout() {
-  return (
-    <div>
-      <h1>Logout Page</h1>
-    </div>
-  );
+  const { user, setUser } = useContext(AppContext);
+  const Navigate = useNavigate();
+  useEffect(() => {
+    setUser({});
+    Navigate("/");
+  }, []);
+  return <h2>Logout Page</h2>;
 }
-export default Logout
+export default Logout;
